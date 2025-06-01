8e93f5b01fca0ab5073ec9c72fa24ec83d6a88f0
Star <101126792+Starx707@users.noreply.github.com>
2025-06-01 22:58:04 +0200

    small change

--- src/js/glasses.js
+++ src/js/glasses.js
@@ -5,7 +5,6 @@ export class Glasses extends Actor {
     constructor(){ 
         super();
         this.pos = new Vector(3,-1)
-        // this.graphics.use(Resources.Glasses.toSprite());
         this.graphics.use(Resources.PinkGlasses.toSprite());
         this.scale = new Vector(0.4,0.4)
     }
